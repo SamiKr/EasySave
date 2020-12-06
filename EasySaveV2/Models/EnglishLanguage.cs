bb2bfe4784952daae8bd9970fc5f97ba3336daa2
Sami Kosbur <sami.kosbur@viacesi.fr>
2020-12-06 17:07:21 +0100

    Multilanguage

--- EasySaveV2/Models/EnglishLanguage.cs
+++ EasySaveV2/Models/EnglishLanguage.cs
@@ -5,5 +5,18 @@ namespace EasySaveV2.Models
     public class EnglishLanguage : ILanguage
     {
         public string MySaves => "My saves";
+        public string Name => "Name";
+        public string pathSource => "pathSource";
+        public string pathDestination => "pathDestination";
+        public string Type => "Type";
+        public string Crypter => "Crypter";
+        public string Launch => "Launch the saves";
+        public string SavesTypes => "Saves Types";
+        public string Add => "Add";
+        public string saveName => "Save Name";
+        public string saveChecked => "Save Checked";
+        public string welcome => "Welcome to parameters";
+
+
     }
 }
--- EasySaveV2/Models/FrenchLanguage.cs
+++ EasySaveV2/Models/FrenchLanguage.cs
@@ -4,6 +4,17 @@ namespace EasySaveV2.Models
 {
     public class FrenchLanguage : ILanguage
     {
-        public string MySaves => "Mes sauvegardes";
+        public string MySaves => "My saves";
+        public string Name => "Name";
+        public string pathSource => "pathSource";
+        public string pathDestination => "pathDestination";
+        public string Type => "Type";
+        public string Crypter => "Crypter";
+        public string Launch => "Launch the saves";
+        public string SavesTypes => "Saves Types";
+        public string Add => "Add";
+        public string saveName => "Save Name";
+        public string saveChecked => "Save Checked";
+        public string welcome => "Welcome to parameters";
     }
 }
